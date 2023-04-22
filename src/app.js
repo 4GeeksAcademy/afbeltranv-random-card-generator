1559a366910829c2d1ef33816c61a964a9b50327
afbeltranv <124716013+afbeltranv@users.noreply.github.com>
2023-04-22 02:33:08 +0000

    Se agregan los archivos y la funcionalidad

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const palo = ["♦", "♥", "♠", "♣"];
+  const numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let numeroRnd = document.querySelector(".numero");
+  numeroRnd.textContent = numero[Math.floor(Math.random() * numero.length)];
+  let paloArr = document.querySelector("#palo1");
+  let paloAbj = document.querySelector("#palo2");
+  let paloRnd = palo[Math.floor(Math.random() * palo.length)];
+  paloArr.textContent = paloRnd;
+  paloAbj.textContent = paloRnd;
+  if (paloRnd == "♥") {
+    paloArr.style.color = "red";
+    paloAbj.style.color = "red";
+  }
 };
